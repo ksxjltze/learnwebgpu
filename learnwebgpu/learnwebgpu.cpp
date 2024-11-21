6b60851899523527fe9671998287e66f3f4e3c68
Lee Jia Keat <leejiakeat@gmail.com>
2024-11-21 16:23:34 +0800

    initial

---
+++ learnwebgpu/learnwebgpu.cpp
@@ -0,0 +1,12 @@
+﻿// learnwebgpu.cpp : Defines the entry point for the application.
+//
+
+#include "learnwebgpu.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
